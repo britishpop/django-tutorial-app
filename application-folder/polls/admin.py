f466873d1eea4d1144dbcd4d0107dbe4763bd1d9
britishpop <drmakc@gmail.com>
2019-06-29 15:02:57 +0300

    tutorial finished

--- application-folder/polls/admin.py
+++ application-folder/polls/admin.py
@@ -3,5 +3,26 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- application-folder/polls/models.py
+++ application-folder/polls/models.py
@@ -9,7 +9,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        
+        now = timezone.now()
+        return self.pub_date >= now - datetime.timedelta(days=1)
+        
+    was_published_recently.admin_order = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
     def __str__(self):
         return self.question_text
